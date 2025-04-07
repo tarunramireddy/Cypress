1bb6b3c9435d3f950918d9ec6a792c07b0680f39
Tarun Ramireddy <tarunramireddy@gmail.com>
2025-04-06 23:25:02 -0400

    Added Assertions

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -1,14 +1,31 @@
+const { assert } = require("chai");
+
 describe('First Test Suite', () => {
 
   // Test case 1
   it('first Test', () => {
-    cy.visit('http://www.automationpractice.pl/index.php');
+    cy.visit('http://www.automationpractice.pl/index.php')
+    cy.url().should('include', 'automationpractice')
+    .and('eq', 'http://www.automationpractice.pl/index.php')
+   .and('not.include', 'automationpractice1');
     // cy.get("#search_query_top").type("tops{enter}");
+    cy.get("#search_query_top").should('be.visible');
     cy.xpath("//input[@id='search_query_top']").type("tops{enter}");
+    // cy.xpath("//a").should("have.length", 7);
+    cy.xpath("//input[@id='search_query_top']").should('have.value', 'tops');
   })
  // Test case 2
 it('Second Test', () => {
   cy.visit('http://www.automationpractice.pl/index.php');
+  let txt = "My orders";
+
+  cy.xpath("//a[@title='My orders']").then((x) =>{
+    const txt = x.text();
+    cy.log(txt);
+    // expect(txt).to.equal("My orders");
+    assert.equal(txt, "My orders", "Text is equal");
+  })
+
   cy.get("a[title='My orders']").click();
 })
 })
